645736d1e9a1832ee05f6e13ad45bdea4f13f6b1
Santosh Manoharan <santosh.mano@gmail.com>
2021-02-16 14:12:58 -0800

    Leetcode, problem 448
    https://leetcode.com/problems/find-all-numbers-disappeared-in-an-array/

---
+++ leetcode/448_find_all_numbers_dissapeared_in_an_array.go
@@ -0,0 +1,46 @@
+package leetcode
+
+// counting sort technique, O(1) space, O(n) time
+func findDisappearedNumbers(nums []int) []int {
+
+	for i := 0; i < len(nums); i++ {
+		for nums[i] != i+1 && nums[i] != nums[nums[i]-1] {
+			nums[i], nums[nums[i]-1] = nums[nums[i]-1], nums[i]
+		}
+	}
+
+	var elemsNotPresent []int
+
+	for i, val := range nums {
+		if val != i+1 {
+			elemsNotPresent = append(elemsNotPresent, i+1)
+		}
+	}
+
+	return elemsNotPresent
+}
+
+// O(n) time, O(n) space, quick soln
+func findDisappearedNumbersSimple(nums []int) []int {
+
+	m := make(map[int]int)
+
+	// initialize the map
+	for i := 1; i <= len(nums); i++ {
+		m[i-1] = 0
+	}
+
+	for _, val := range nums {
+		m[val-1] += 1
+	}
+
+	var elemsNotPresent []int
+
+	for key, val := range m {
+		if val == 0 {
+			elemsNotPresent = append(elemsNotPresent, key+1)
+		}
+	}
+
+	return elemsNotPresent
+}
